83e2b22d5af69c2d23d818a98d3b3e9ddf6bdd67
AstracodeLAB <astracodelab@gmail.com>
2024-07-09 13:39:46 +0200

    Add components, pages, and first images

---
+++ lumina-look/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Lumina Look</h1>
+        
+    </>
+  )
+}
+
+export default App
--- lumina-look/src/main.jsx
+++ lumina-look/src/main.jsx
@@ -7,6 +7,5 @@ import './i18n';
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+	</React.StrictMode>
 );
