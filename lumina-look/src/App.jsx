2edf223a7e3ae7265b75b9731f1dbe7f3f10657a
AstracodeLAB <astracodelab@gmail.com>
2024-07-09 13:49:05 +0200

    Add routes with react-router-dom and App.js structure

--- lumina-look/src/App.jsx
+++ lumina-look/src/App.jsx
@@ -1,14 +1,20 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
 
 function App() {
-  
-
-  return (
-    <>
-      <h1>Lumina Look</h1>
-        
-    </>
-  )
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Home />}></Route>
+				<Route path='/about' element={<About />}></Route>
+				<Route path='/services' element={<Services />}></Route>
+				<Route path='/' element={<Contact />}></Route>
+			</Routes>
+		</Router>
+	);
 }
 
-export default App
+export default App;
