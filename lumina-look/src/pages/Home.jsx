cc5d9d335a12fd9ed82a6487a75b5d5953ab2066
AstracodeLAB <astracodelab@gmail.com>
2024-07-09 13:56:27 +0200

    Languages config

--- lumina-look/src/pages/Home.jsx
+++ lumina-look/src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 function home() {
-  return (
-    <>
-     <h1>HOME</h1> 
-    </>
-  )
+  const {t} = useTranslation();
+
+	return (
+		<>
+			<h1>{t('home.title')}</h1>
+      <p>{t('home.description')}</p>
+		</>
+	);
 }
 
-export default home
+export default home;
