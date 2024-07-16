095ceaf570ef5d6786fceca5721a682375c77930
AstracodeLAB <astracodelab@gmail.com>
2024-07-16 20:11:36 +0200

    Add Services page design for tablet and desktop sizes

--- lumina-look/src/pages/Services.jsx
+++ lumina-look/src/pages/Services.jsx
@@ -5,31 +5,34 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { useTranslation } from 'react-i18next';
 import services from '../data/servicesData';
 
-
 function Services() {
-	const { t } = useTranslation();
+    const { t } = useTranslation();
 
-	return (
-		<>
-			<Header />
-			<div className='max-w-lg mx-auto px-4 py-12'>
-				{services.map((service) => (
-					<section key={service.id} id={service.id} className='mb-8'>
-						<h2 className='text-3xl text-center font-semibold mb-4'>{t(service.title)}</h2>
-						<Carousel showThumbs={false} showStatus={false} infiniteLoop>
-							{service.images.map((image, index) => (
-								<div key={index}>
-									<img src={image.src} alt={image.alt} />
-								</div>
-							))}
-						</Carousel>
-						<p className='text-lg text-center mt-4 p-4'>{t(service.description)}</p>
-					</section>
-				))}
-			</div>
-			<Footer />
-		</>
-	);
+    return (
+        <>
+            <Header />
+            <div className='max-w-7xl mx-auto px-4 py-12'>
+                {services.map((service, index) => (
+                    <section key={service.id} id={service.id} className={`mb-8 flex flex-col md:flex-row items-center ${index % 2 === 0 ? 'md:flex-row-reverse' : 'md:flex-row'}`}>
+                        <div className='max-w-xs mx-auto md:w-1/2 mb-6 md:mb-10'>
+                            <Carousel showThumbs={false} showStatus={false} infiniteLoop>
+                                {service.images.map((image, index) => (
+                                    <div key={index}>
+                                        <img src={image.src} alt={image.alt} />
+                                    </div>
+                                ))}
+                            </Carousel>
+                        </div>
+                        <div className='max-w-lg mx-auto md:w-1/2'>
+                            <h2 className='text-3xl text-center font-semibold mb-4'>{t(service.title)}</h2>
+                            <p className='text-lg text-center mt-4 p-4'>{t(service.description)}</p>
+                        </div>
+                    </section>
+                ))}
+            </div>
+            <Footer />
+        </>
+    );
 }
 
 export default Services;
