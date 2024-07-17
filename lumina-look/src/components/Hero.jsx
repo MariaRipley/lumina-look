f59c004f7db80d005fe57c5ea4af20355ad512fc
AstracodeLAB <astracodelab@gmail.com>
2024-07-17 09:37:41 +0200

    Remove with from hero images to fix sizing

--- lumina-look/src/components/Hero.jsx
+++ lumina-look/src/components/Hero.jsx
@@ -15,13 +15,13 @@ const services = [
 
 function Hero() {
 	return (
-		<div className='relative bg-cover bg-center' style={{ backgroundImage: `url(${BgPic})` }}>
+		<div className=' relative bg-cover bg-center' style={{ backgroundImage: `url(${BgPic})` }}>
 			<div className='absolute inset-0 bg-black opacity-50'></div>
 			<div className='relative z-10 p-20'>
 				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10'>
 					{services.map((service, index) => (
 						<Link key={index} to={service.path}>
-							<div className='relative w-72 mx-auto'>
+							<div className='relative  mx-auto'>
 								<div>
 									<img src={service.image} alt={service.label} className='w-full h-3/4 object-cover' />
 									<div className='absolute bottom-[-20px] left-1/2 transform -translate-x-1/2 bg-white bg-opacity-75 px-4 py-2'>
