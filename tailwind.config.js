2ec6a41293ec9050e1ec1e052085d124b218f33f
AstracodeLAB <astracodelab@gmail.com>
2024-08-05 18:07:06 +0200

    Remove purge plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {
